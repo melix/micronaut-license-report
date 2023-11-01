465eb4cbc97b8fb3237a0024a4588c234de7e284
Stevo Mitric <stevo.mitric@oracle.com>
2023-11-01 13:11:15 +0100

    Added support for 'checkoutRepo' argument

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,14 @@ plugins {
     id("io.micronaut.build.license-report")
 }
 
+val checkoutRepo : String by project
+
 licenseReports {
+    val checkoutRepositories : List<String> = if (project.hasProperty("checkoutRepo")) checkoutRepo.split(",") else listOf();
+    var repositoryNames : List<String> = checkoutRepositories.map {
+        it.split('@').first();
+    }
+
     listOf(
             "micronaut-core@v3.8.5",
             "micronaut-rxjava3",
@@ -62,6 +69,10 @@ licenseReports {
             "micronaut-maven-plugin",
             "micronaut-camel",
             "micronaut-build-plugins",
+    ).filter {
+        !repositoryNames.contains(it.split('@').first())
+    }.plus(
+        checkoutRepositories
     ).filter {
       it.contains("micronaut-core")
     }.map {
